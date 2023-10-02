c6c829e5536002e8929a602f132c2f9757e603c0
James Lynch <james.lynchie@gmail.com>
2023-10-02 09:44:56 +0100

    QoL Update - Added the traits lookup and also the function to retrieve the traits from the JSON.

--- raw/functions.py
+++ raw/functions.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 def get_data_from_api(api_url):
@@ -15,3 +17,20 @@ def get_data_from_api(api_url):
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         return None
+
+def get_distictlist_from_json():
+    # Parse the JSON data
+    with open('./resources/units.json') as f:
+        # load json
+        data = json.load(f)
+
+    # Extract the list of distinct "specialities" from the units
+    specialities = set()
+    for unit in data["units"]:
+        specialities.update(unit["specialities"])
+
+    # Convert the set of specialities to a list if needed
+    specialities_list = list(specialities)
+
+    # Print the list of distinct specialities
+    print(specialities_list)
