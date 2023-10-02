8cc395418a65482801079c6e5af623e69c270e26
James Lynch <james.lynchie@gmail.com>
2023-10-02 15:25:35 +0100

    Dirty Bug Fix - MySQL Module not installing on linux box, may require rebuild, curbing the DB insert in favour of an archaic log file to sort the inserts at a later date. Dirty fix.

--- raw/sql.py
+++ raw/sql.py
@@ -1,57 +1,85 @@
 import json
 
-import mysql.connector
-from mysql.connector import Error
+#import mysql.connector
+#from mysql.connector import Error
 from dotenv import load_dotenv
 import os
 
 # Load the environment variables from .env file
 load_dotenv()
 
-def establish_connection():
-    try:
-        # Get the connection string from environment variables
-        host = os.getenv('MYSQLDBHOST')
-        database = os.getenv('MYSQLDATABASE')
-        user = os.getenv('MYSQLDBUSER')
-        password = os.getenv('MYSQLDBPWD')
-
-        # Define the connection parameters
-        connection_params = {
-            'host': host,
-            'database': database,
-            'user': user,
-            'password': password
-        }
-
-        # Establish database connection using the connection string
-        connection = mysql.connector.connect(**connection_params)
-        if connection.is_connected():
-            return connection
-    except Error as e:
-        print("Error:", e)
-        return None
-
+# def establish_connection():
+#     try:
+#         # Get the connection string from environment variables
+#         host = os.getenv('MYSQLDBHOST')
+#         database = os.getenv('MYSQLDATABASE')
+#         user = os.getenv('MYSQLDBUSER')
+#         password = os.getenv('MYSQLDBPWD')
+#
+#         # Define the connection parameters
+#         connection_params = {
+#             'host': host,
+#             'database': database,
+#             'user': user,
+#             'password': password
+#         }
+#
+#         # Establish database connection using the connection string
+#         connection = mysql.connector.connect(**connection_params)
+#         if connection.is_connected():
+#             return connection
+#     except Error as e:
+#         print("Error:", e)
+#         return None
+#
+#
+# def add_replay_to_db(filename, gamejson, playerjson, resultjson, replaysource, messageauthor):
+#     try:
+#
+#         connection = establish_connection()
+#
+#         if connection:
+#
+#             # Define the update query
+#             update_query = f"INSERT INTO `yesman`.`replay_raw_data` (`filename`, `game_json`, `player_json`, `result_json`, `replay_source`, `user`) VALUES (%s,%s ,%s ,%s ,%s, %s);"
+#
+#             # Execute the query with parameters
+#             cursor = connection.cursor()
+#             cursor.execute(update_query, (filename, json.dumps(gamejson),json.dumps(playerjson),json.dumps(resultjson),replaysource, messageauthor))
+#
+#             # Commit the changes
+#             connection.commit()
+#
+#         print("Update successful")
+#     except Error as e:
+#         print("Error:", e)
+#     finally:
+#         cursor.close()
 
 def add_replay_to_db(filename, gamejson, playerjson, resultjson, replaysource, messageauthor):
     try:
 
-        connection = establish_connection()
-
-        if connection:
+        #connection = establish_connection()
+        #
+        # if connection:
 
             # Define the update query
-            update_query = f"INSERT INTO `yesman`.`replay_raw_data` (`filename`, `game_json`, `player_json`, `result_json`, `replay_source`, `user`) VALUES (%s,%s ,%s ,%s ,%s, %s);"
+        update_query = f"INSERT INTO `yesman`.`replay_raw_data` (`filename`, `game_json`, `player_json`, `result_json`, `replay_source`, `user`) VALUES (%s,%s ,%s ,%s ,%s, %s);"
 
-            # Execute the query with parameters
-            cursor = connection.cursor()
-            cursor.execute(update_query, (filename, json.dumps(gamejson),json.dumps(playerjson),json.dumps(resultjson),replaysource, messageauthor))
+        with open("db_log.txt", "a") as file:
+            # Append the variable's value to the file
+            file.write(update_query + "\n")
 
-            # Commit the changes
-            connection.commit()
-
-        print("Update successful")
-    except Error as e:
-        print("Error:", e)
-    finally:
-        cursor.close()
+            # # Execute the query with parameters
+            # cursor = connection.cursor()
+            # cursor.execute(update_query, (filename, json.dumps(gamejson),json.dumps(playerjson),json.dumps(resultjson),replaysource, messageauthor))
+            #
+            # # Commit the changes
+            # connection.commit()
+    except Exception as e:
+        print(e)
+    #     print("Update successful")
+    # except Error as e:
+    #     print("Error:", e)
+    # finally:
+    #     cursor.close()
