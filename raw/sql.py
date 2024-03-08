fe45791ed4632c9144a17b91a1ab455b44ee7035
James Lynch <james.lynchie@gmail.com>
2024-03-08 10:54:14 +0000

    Update - Removed the LB Processing from the Bot into a separate program, and centralised the SQLite db, also placed a timeout on the replay command.

--- raw/sql.py
+++ raw/sql.py
@@ -7,7 +7,7 @@ logger = logging.getLogger('logger')
 
 def get_database():
     try:
-        db_path = './database/yesman.db'
+        db_path = '../database/yesman.db'
 
         # Ensure the directory exists
         if not os.path.exists(os.path.dirname(db_path)):
@@ -26,7 +26,7 @@ def add_replay_to_db(filename: str, gamejson: str, playerjson: str, resultjson: 
 
         logger.info(f'Adding {filename} to the database')
 
-        connection = sqlite3.connect(get_database())
+        connection = sqlite3.connect(get_database(), timeout=30)
         cursor = connection.cursor()
 
         cursor.execute('''CREATE TABLE IF NOT EXISTS tbl_replay_raw_data (
@@ -45,57 +45,6 @@ def add_replay_to_db(filename: str, gamejson: str, playerjson: str, resultjson: 
             'INSERT INTO tbl_replay_raw_data (filename, game_json, player_json, result_json, replay_source, user, md5) VALUES (?, ?, ?, ?, ?, ?, ?)',
             (str(filename), str(gamejson), str(playerjson), str(resultjson), str(replaysource), str(messageauthor),
              str(md5)))
-
-        connection.commit()
-
-    except sqlite3.Error as er:
-        logger.error(er)
-
-    finally:
-        if connection:
-            connection.close()
-
-
-def add_usernames_to_db(data):
-    try:
-        logger.debug(f'Adding usernames to the database')
-
-        connection = sqlite3.connect(get_database())
-        cursor = connection.cursor()
-
-        cursor.execute('''CREATE TABLE IF NOT EXISTS tbl_usernames (
-                                    id INTEGER PRIMARY KEY,
-                                    rank TEXT,
-                                    elo TEXT,
-                                    userid TEXT,
-                                    username TEXT,
-                                    start_date DATE,
-                                    end_date DATE,
-                                    is_current INTEGER
-                                  )''')
-
-        # Create a trigger to manage updates
-        cursor.execute('''
-            CREATE TRIGGER IF NOT EXISTS trg_update_usernames
-            BEFORE INSERT ON tbl_usernames
-            BEGIN
-                UPDATE tbl_usernames
-                SET end_date = DATETIME('now'),
-                    is_current = 0
-                WHERE userid = NEW.userid AND is_current = 1;            
-            END;
-        ''')
-
-        cursor.executemany('''
-            INSERT INTO tbl_usernames (rank, elo, userid, username, start_date, end_date, is_current)
-            SELECT ?, ?, ?, ?, DATETIME('now'), '9999-12-31', 1
-            WHERE NOT EXISTS (
-                SELECT 1
-                FROM tbl_usernames
-                WHERE userid = ? AND is_current = 1 AND username = ?
-            )
-        ''', [(row['rank'], row['elo'], row['userid'], row['username'], row['userid'], row['username']) for row in
-              data])
 
         connection.commit()
 
